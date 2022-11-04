b34a5c8d87e6df2b2bec0bb0dc3ca82f1a4e32ec
goSciuGH <49696410+goSciuGH@users.noreply.github.com>
2022-11-04 10:24:55 +0100

    Resolution update

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,11 +1,18 @@
 #include <SFML/Graphics.hpp>
 
+int FULLHD_W = 1920;
+int FULLHD_H = 1080;
+
 int main()
 {
-	int windowWidth = 640;
-	int windowHeight = 360;
+	bool fullScreen = false;
+	
+	double windowScale = 1.0/3.0;
 
-	sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "SFML works!", sf::Style::Titlebar | sf::Style::Close);
+	int windowW = FULLHD_W * windowScale;
+	int windowH = FULLHD_H * windowScale;
+
+	sf::RenderWindow window(sf::VideoMode(windowW, windowH), "Tetris PSk", sf::Style::Titlebar | sf::Style::Close);
 	window.setVerticalSyncEnabled(true);
 
 	//sf::CircleShape shape(100.f);
@@ -19,10 +26,12 @@ int main()
 
 	sf::Sprite spr_Matrix;
 	spr_Matrix.setTexture(ttr_Matrix);
+	spr_Matrix.setScale(windowScale, windowScale);
 	sf::Vector2u matrixSize = ttr_Matrix.getSize();
-	int matrixPosX = windowWidth / 2 - matrixSize.x / 2;
-	int matrixPosY = windowHeight / 2 - matrixSize.y / 2;
+	int matrixPosX = windowW / 2.0 - matrixSize.x * windowScale / 2.0;
+	int matrixPosY = windowH / 2.0 - matrixSize.y * windowScale / 2.0;
 	spr_Matrix.setPosition(matrixPosX, matrixPosY);
+	//spr_Matrix.setPosition(200, 20);
 
 	while (window.isOpen())
 	{
